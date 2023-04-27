24ba64f88de6ee21e9d34c9ce543a28152544bd9
Abdul Sayed <sayed.abdul2595@gmail.com>
2023-04-27 16:55:58 +0200

    Add initial implementation of getCategories handler

---
+++ src/handlers/getCategoriesHandler.ts
@@ -0,0 +1,32 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import { defaultHeaders, loadSsmParameter } from '../Utils'
+
+export const getCategoriesHandler = async function(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
+    let categories = await loadSsmParameter("balance-categories")
+
+    if (categories == null) {
+        let responseBody: ErrorResponse = {
+            httpStatus: 500,
+            message: "Could not retrieve categories from SSM"
+        }
+
+        return {
+            statusCode: 500,
+            headers: defaultHeaders,
+            body: JSON.stringify(responseBody)
+        }
+    } else {
+        let responseBody: Array<Category> = categories.split(',').map(category => { 
+            return <Category> {
+                name: category, 
+                activities: [] 
+            }
+        })
+
+        return {
+            statusCode: 200,
+            headers: defaultHeaders,
+            body: JSON.stringify(responseBody)
+        }
+    }
+}
